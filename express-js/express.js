0390a05b8ccd1812d4b9b9e7882601f15e1aeb58
sidereior <alexanderlnanda@gmail.com>
2022-09-03 10:35:36 -0400

    finished endpoints for vault

--- express-js/express.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-var things = require('C:\\Users\\alexa\\Downloads\\express\\thing.js');
-
-
-app.use('C:\\Users\\alexa\\Downloads\\express\\thing.js', things);
-
-app.listen(3000);
